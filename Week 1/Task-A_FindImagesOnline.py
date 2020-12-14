0323fd23d63e829e14a2dc90818ba0e387408f36
lchangGH <leniechang@gmail.com>
2020-12-13 20:51:36 -0500

    added 2020 LabVIEW link to task A

--- Week 1/Task-A_FindImagesOnline.py
+++ Week 1/Task-A_FindImagesOnline.py
@@ -5,3 +5,4 @@
 # Please paste the found links into this file below, but in your cloned repository
 
 # Choose a few images found online, say 10 to 15 and create a sub-folder in your cloned repository
+https://forums.ni.com/t5/FIRST-Robotics-Competition/2020-LabVIEW-for-FRC-Vision-Examples/ta-p/4003680?profile.language=en
