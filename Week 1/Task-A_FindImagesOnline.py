7954fba4f7dd1b7d6b700fc5dea944629c3399d6
brindoro <brindoro@gmail.com>
2020-12-13 20:51:35 -0500

    added links to task A

--- Week 1/Task-A_FindImagesOnline.py
+++ Week 1/Task-A_FindImagesOnline.py
@@ -5,3 +5,5 @@
 # Please paste the found links into this file below, but in your cloned repository
 
 # Choose a few images found online, say 10 to 15 and create a sub-folder in your cloned repository
+https://docs.wpilib.org/en/stable/docs/software/vision-processing/introduction/2017-vision-examples.html
+https://www.kevinhughes.ca/blog/FRC-Ultimate-Ascent-Vision-System.html
