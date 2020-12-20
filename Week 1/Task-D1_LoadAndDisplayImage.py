5b726b9f7003839ea0f8dfeb0ba4bb31c4bb4522
Jdiep7 <jdiep3@ocdsb.ca>
2020-12-19 21:33:24 -0500

    Completed task1 D1

---
+++ Week 1/Task-D1_LoadAndDisplayImage.py
@@ -0,0 +1,30 @@
+# This is a pseudo code file for Merge Robotics, 2021, Game Changers
+
+# This is task D1 - > Load and Display Images
+
+# Using Python and OpenCV, write a small bit of code to load an image and display it on your screen.
+
+# Recommeded starting points 
+#    -> https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_tutorials.html
+
+# To help we have comments to prompt how to do this is below
+
+# Imports!
+# Python - import modules of code as required (OpenCV here)
+import numpy as np
+import cv2
+# define a string variable for the path to the file
+a = r"C:\Users\Jamie Diep\Documents\frc2021\Vision Images\LED Peg\1ftH1ftD0Angle0Brightness.jpg"
+print (a)
+
+# load a color image using string
+img = cv2.imread(a,cv2.IMREAD_COLOR)
+
+# display the color image to screen
+cv2.imshow('image',img)
+
+# wait for user input to close
+cv2.waitKey(0)
+
+# cleanup and exit
+cv2.destroyAllWindows()
