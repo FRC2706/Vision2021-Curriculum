0e8d4efe58f6982d998519a4fd5f821c738e91ce
Elijah <elijah@thevandyks.ca>
2020-12-20 19:17:08 -0500

    images direct link

--- Week 1/Task-A_FindImagesOnline.py
+++ Week 1/Task-A_FindImagesOnline.py
@@ -1,6 +1,6 @@
 
-https://www.chiefdelphi.com/t/pic-frc-971-vision-tracking-screen-shot/183737
-https://docs.limelightvision.io/en/latest/theory.html
+https://www.chiefdelphi.com/uploads/default/original/3X/b/e/bec1fa09df07e79418bd69603e6ac287e460dc8f.jpeg
+https://docs.limelightvision.io/en/latest/_images/VisionTargetExample2.jpg
 https://www.firstinspires.org/robotics/frc/playing-field
-https://www.chiefdelphi.com/t/kickoff-release-of-wpilib-frcvision-raspberry-pi-image/335273
+https://www.chiefdelphi.com/uploads/default/original/3X/e/1/e112521d1d07fac9271c9d492a924016188a614b.jpeg
 
