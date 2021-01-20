2b716e0ca44e673443fee3ff94dbff4c6db6a5ae
IntegerConversionError <ekaya1@ocdsb.ca>
2021-01-20 18:08:54 -0500

    Added masking method

--- Week 2/Task-F_SimplifedEnhancedMaskingMethods.py
+++ Week 2/Task-F_SimplifedEnhancedMaskingMethods.py
@@ -18,5 +18,24 @@
 # Objective is to pick a method to use for making our mask.
 
 # Post a few favourie links and code snippets into a python copy of this file, then push to github.
+#*
+def threshold_range(im, lo, hi):
+    unused, t1 = cv2.threshold(im, lo, 255, type=cv2.THRESH_BINARY)
+    unused, t2 = cv2.threshold(im, hi, 255, type=cv2.THRESH_BINARY_INV)
+    return cv2.bitwise_and(t1, t2)
 
+#no pictures for 34-54, 10
+img = cv2.imread('RealFullField/79.jpg')
+cv2.imshow('image', img)
 
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+h, s, v = cv2.split(hsv)
+a, b = 2, 1
+h = threshold_range(h, 63, 105)
+s = threshold_range(s, 7, 255)
+v = threshold_range(v, 67, 242)
+#cv2.imshow('s', s)
+combined = cv2.bitwise_and(h, cv2.bitwise_and(s,v))
+#img2 = combined.copy()
+contours, v = cv2.findContours(combined, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
