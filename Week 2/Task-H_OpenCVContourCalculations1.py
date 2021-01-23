62122e6efb62ee2b3ca23bb5403510ed97e120ce
lchangGH <leniechang@gmail.com>
2021-01-23 17:21:29 -0500

    Distance and Contour Calculations done

---
+++ Week 2/Task-H_OpenCVContourCalculations1.py
@@ -0,0 +1,15 @@
+# This is a pseudo code file for Merge Robotics, 2021, Game Changers
+
+# This is task H - > OpenCV "Contour Calculations."  Not sure if it is clear by now, 
+# but OpenCV can do a lot of things, we need to understand what it offers to complete 
+# our vision code.  For a given single contour, (meaning it was imaged and masked and 
+# converted to a coordinate array), you need to be able to use a number of OpenCV functions.
+# Please experiment with the following, easiest is to simply draw them back to a blank image
+# or on top of original.
+
+# --> moments, contour area, contour perimeter, contour approximation, bounding rectangles, 
+# minimum enclosing circle, fitting elipse, fitting line, etc.
+
+# useful links
+# https://docs.opencv.org/4.5.0/dd/d49/tutorial_py_contour_features.html
+# https://docs.opencv.org/4.5.0/d1/d32/tutorial_py_contour_properties.html
